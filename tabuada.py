af84f47975c0588a039a6c6ef73ae57eaa643679
SALIM1BERTO <julhohu23@outlook.com>
2026-03-17 22:22:02 -0300

    alterei a indentação

--- tabuada.py
+++ tabuada.py
@@ -1,5 +1,7 @@
 print(f"{' TABUADA ':=^40}\n")
+
 n1 = int(input("Digite um número para ver a tabuada: "))
 print(f"\n{' TABUADA DO ' + str(n1) :^40}\n")
+
 for i in range(1, 11):
     print(f"{n1} x {i} = {n1 * i}")
