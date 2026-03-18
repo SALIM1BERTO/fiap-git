0eb12a4071818662a9ffbe14db7f8e06f58e8b48
SALIM1BERTO <julhohu23@outlook.com>
2026-03-17 23:06:21 -0300

    troquei o sistema if por match case

--- calculadoraPorcent.py
+++ calculadoraPorcent.py
@@ -1,25 +1,37 @@
+import os
+os.system("cls")
+
 print("Vamos calcular porcentagem!")
 
-escolha = int(input("Digite:\n"
-                "1 para saber a porcentagem\n"
-                "2 para saber um desconto\n"
-                "3 para saber um acrecimo\n"
-                ">"))
+# escolha = int(input("Digite:\n"
+#                 "1 para saber a porcentagem\n"
+#                 "2 para saber um desconto\n"
+#                 "3 para saber um acrecimo\n"
+#                 ">"))
+#
+# if escolha not in (1, 2, 3):
+#     print("Opção invalida")
+#     exit()
 
-if escolha not in (1, 2, 3):
-    print("Opção invalida")
-    exit()
+print(f"""
+1 - saber porcentagem
+2 - saber um desconto
+3 - saber um acrecimo
+""")
+
+escolha = int(input("Escolha uma opção: "))
 
 por = float(input("Digite a porcentagem: "))
 valor = float(input("\nDigite o valor: "))
 
 porc = por / 100 * valor
 
-if escolha == 1:
-    print(f"\nA porcentagem de {valor} é {porc}")
+match escolha:
+    case 1:
+        print(f"\n{por}% de {valor} é {porc}")
 
-elif escolha == 2:
-    print(f"O valor de {valor} com deseconto de {por}% fica: {valor - porc}")
+    case 2:
+        print(f"O valor de {valor} com deseconto de {por}% fica: {valor - porc}")
 
-elif escolha == 3:
-    print(f"O valor de {valor} com acrecimo de {por}% fica: {valor + porc}")
+    case 3:
+        print(f"O valor de {valor} com acrecimo de {por}% fica: {valor + porc}")
---
+++ fast tests/q1.py
@@ -0,0 +1,13 @@
+b = int(100)
+
+if b != 50 * 2:
+    b = b - 300
+
+else:
+    b = b + 300
+    b = b + 1
+
+if b == 301:
+    b = b + 2
+
+print("Resposta = {0}".format(b))
