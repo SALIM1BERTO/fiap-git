cd315ef8f6e8a29c59e5b2ddcc3bc9fcbf4314d0
SALIM1BERTO <julhohu23@outlook.com>
2026-03-20 11:15:08 -0300

    serviço

---
+++ feitos_serviço/somainfinitaatezero.py
@@ -0,0 +1,21 @@
+# import os
+# os.system('cls')
+
+# n = float(input("Digite um numero: "))
+# soma1 = float(input("Agora digite um numero para somar: "))
+
+# print("Quando quiser parar digite 0\n")
+
+# while True:
+#     soma = float(input("> "))
+#     n = n + soma
+
+#     if soma == 0:
+#         n = n + soma1
+#         break
+
+# print(f"Sua soma ficou: {n}")
+
+for i in range(1, 11, 1):
+    print(f"{i}", end = ', ')
+    i += 10
--- informa;óes.py
+++
@@ -1,9 +0,0 @@
-nome = input("Qual seu nome: ")
-idade = int(input("Qual sua idade: "))
-comida = input("Qual sua comida favorita: ")
-
-print("Juntando tudo fica\n")
-
-print(f"{"Seu nome é:":<30}{nome:>15}")
-print(f"{'Sua idade é:':<30}{idade:>15}")
-print(f"{'Sua comida favorita é:':<30}{comida:>15}")
